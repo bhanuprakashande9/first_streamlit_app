d05c2eed02e825ffd288395576c8fea3e788e2b0
bhanuprakashande9 <128985708+bhanuprakashande9@users.noreply.github.com>
2023-03-27 09:34:36 +0530

    Update streamlit_app.py
    Removed old text and hedder and added this new text and hedder
    
    streamlit.header('Breakfast Menu')
    streamlit.text('Omega 3 & Blueberry Oatmeal')
    streamlit.text('Kale, Spinach & Rocket Smoothie')
    streamlit.text('Hard-Boiled Free-Range Egg')

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-streamlit.header('Break fast menu')
-streamlit.text('Spinach and hard boiled egg')
-streamlit.text('and some green tea')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
 
 
